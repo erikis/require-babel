55a9ef98ac94f741498911e82c95f10773155c7f
Erik Isaksson <erikis@kth.se>
2020-05-03 14:28:53 +0200

    Add JSON and text loader plugins json.js and text.js

---
+++ json.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2020 Erik Isaksson
+ * Released under MIT license, https://github.com/erikis/require-babel/blob/master/LICENSE
+ */
+
+/*global define, JSON */
+
+define(['module', 'require-babel'], function (module, babel) {
+    'use strict';
+
+    var pluginConfig = module.config ? module.config() : {};
+    pluginConfig.extension = pluginConfig.extension || '.json';
+    pluginConfig.extensions = pluginConfig.extensions || {};
+    pluginConfig.transform = function (text) {
+        return JSON.parse(text);
+    };
+
+    return {
+        version: babel.version,
+
+        write: function (pluginName, name, write) {
+            return babel.write.call(babel, pluginName, name, write);
+        },
+
+        load: function (name, parentRequire, load, config) {
+            return babel.load.call(babel, name, parentRequire, load, config, pluginConfig);
+        }
+    };
+});
