4f97e11994ddb333e334098e2053b36b7a09d56e
Erik Isaksson <erikis@kth.se>
2020-05-03 11:06:10 +0200

    Add TypeScript loader plugins tsm.js and tsx.js

---
+++ tsm.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2020 Erik Isaksson
+ * Released under MIT license, https://github.com/erikis/require-babel/blob/master/LICENSE
+ */
+
+/*global define */
+
+define(['module', 'require-babel'], function (module, babel) {
+    'use strict';
+
+    var pluginConfig = module.config ? module.config() : {};
+    pluginConfig.extension = pluginConfig.extension || '.ts';
+    pluginConfig.extensions = pluginConfig.extensions || { '.ts': 'tsm!', '.tsx': 'tsx!' };
+    var options = pluginConfig.options = pluginConfig.options || {};
+    var presets = babel.defaultPresets.slice();
+    presets.push([ 'typescript', pluginConfig.typescript || { allExtensions: true } ]);
+    options.presets = options.presets || presets;
+
+    return {
+        version: babel.version,
+
+        write: function(pluginName, name, write) {
+            return babel.write.call(babel, pluginName, name, write);
+        },
+
+        load: function (name, parentRequire, load, config) {
+            return babel.load.call(babel, name, parentRequire, load, config, pluginConfig);
+        }
+    };
+});
